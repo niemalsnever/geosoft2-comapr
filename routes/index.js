063c3279729fe26b42b97a524c2c7a9b1593612e
SulaxanSo <sulaxan.so@live.de>
2017-01-26 16:24:23 +0100

    included router.get(edit)

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,24 @@ router.get('/my-account', function (req, res) {
     }
 });
 
+router.get('/edit', function (req, res) {
+   if(req.user) {
+       //console.log(req.user.id);
+       var user;
+       db_functions.getUser(req.user.id, function (err, row) {
+           user = row;
+           res.render('edit', { title: 'My Account - ' + user.name + ' (' + user.email + ')', user: user });
+       });
+   }
+   else {
+       //res.status(403).send();
+       res.status(403).render('error',  {error: {
+           status: 403,
+           msg: 'Sorry, you are not logged in. Please click here to get back to the <a href="/">Login page</a>'
+       } })
+   }
+});
+
 router.get('/map-view', function (req, res) {
     if(req.user) {
         //console.log(req.user);
